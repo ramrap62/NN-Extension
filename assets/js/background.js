4fe28718a9b915e52ece149899adfb1221953416
Sai Vinay <48785726+CapedCrusader505@users.noreply.github.com>
2019-12-26 15:38:44 +0530

    Update background.js

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,25 +1,12 @@
 chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
-  console.log("Hello again!");
   if(message.msg === "image"){
-    console.log("Background");
-    console.log(JSON.stringify(message.myurl, undefined, 2)); 
+    //console.log(JSON.stringify(message.myurl, undefined, 2)); 
     //fetch('http://ai.bpraneeth.com:6788/nudenet_classifier_url?url='+message.myurl)
     fetch('http://localhost:5004/nudenet?url='+message.myurl)
           .then(response => response.text())
           .then(data => {
-            console.log("before parsing");
             let dataObj = JSON.parse(data);
-            var lookup = [];
-            var i = 0;
-            for(var name in dataObj){
-              if(dataObj.hasOwnProperty(name)){
-                lookup[i] = dataObj[name];
-                i++;
-              }
-            }
             console.log(JSON.stringify(dataObj));
-            console.log(lookup[0]);
-            //senderResponse({data: lookup[0], index: message.index});
             senderResponse({data: dataObj, index: message.index});
           })
           .catch(error => console.log("error", error))
