ded78e8848dca736e787c63f4b5a50beee4ea49d
vinay.s.bellamkonda@gmail.com <vinay.s.bellamkonda@gmail.com>
2020-01-31 09:28:02 +0530

    added Login_v4

--- background.js
+++ background.js
@@ -21,7 +21,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, senderResponse){
   if(message.msg === "image"){
     //console.log(JSON.stringify(message.myurl, undefined, 2)); 
     //fetch('http://ai.bpraneeth.com:6788/nudenet_classifier_url?url='+message.myurl)
-    fetch('http://localhost:5004/nudenet?url='+message.myurl)
+    fetch('http://117.198.102.142:5004/nudenet?url='+message.myurl)
           .then(response => response.text())
           .then(data => {
             let dataObj = JSON.parse(data);
